c647e4814145b6e137929bfa8cfd7c238e6db0a5
vyraun <viraun@microsoft.com>
2018-01-17 19:43:55 +0530

    set up

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from flask import Flask, jsonify
 from flask import request
-from sklearn.externals import joblib
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Please use: http://127.0.0.1:8863/predict?query="
 
-@app.route('/predict', methods=['GET'])
+@app.route('/predict', methods=['POST'])
 def predict():
      json_ = request.json
-     query = "a string after parsing json" # have to clean the request json
-     prediction = [" we will predict something", "like two-three times" ]  #clf.predict(query)
+     query = request.args.get('query') # have to clean the request json
+     prediction = [ query, " we will predict something", "like two-three times" ]  #clf.predict(query)
      return jsonify({'prediction': list(prediction)})
 
 if __name__ == '__main__':
